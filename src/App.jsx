5f850eb70bc9a3599705f009aa1801c0b7abdc54
Khadija12319 <khadijaatul15-12319@diu.edu.bd>
2024-03-24 01:40:45 +0600

    refrence uncontrolled element

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import RefForm from './components/RefForm/RefForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 // import StatefulForm from './components/StatefulForm/StatefulForm'
 
@@ -9,6 +10,7 @@ function App() {
       <h1 className='text-center text-4xl font-bold h-[300px] flex justify-center items-center'>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
+      <RefForm></RefForm>
       </>
   )
 }
--- src/components/RefForm/RefForm.jsx
+++ src/components/RefForm/RefForm.jsx
@@ -1,15 +1,36 @@
+import { useEffect, useRef } from "react";
+
 const RefForm = () => {
     const handleSubmit= (e) =>{
         e.preventDefault();
+        console.log(refName.current.value);
+        console.log(refEmail.current.value);
+        console.log(refPhone.current.value);
+        console.log(refPassword.current.value);
     }
-    return (
+
+    useEffect( () => {
+        refName.current.focus();
+        refEmail.current.focus();
+        refPhone.current.focus();
+        refPassword.current.focus();
+    },[])
+
+    const refName = useRef(null);
+    const refEmail = useRef(null);
+    const refPassword =useRef(null);
+    const refPhone =useRef(null);
+
+    return(
         <div className="text-center">
              <form className="space-y-3" onSubmit={handleSubmit}>
-                <input type="text" name="name" className="border-[2px] border-black p-3 w-80"/>
+                <input ref={refName} type="text" name="name" className="border-[2px] border-black p-3 w-80"/>
                 <br></br>
-                <input type="text" name="email"  className="border-[2px] border-black p-3 w-80"/>
+                <input ref={refEmail} type="text" name="email"  className="border-[2px] border-black p-3 w-80"/>
                 <br></br>
-                <input type="text" name="phone"  className="border-[2px] border-black p-3 w-80"/>
+                <input ref={refPhone} type="text" name="phone"  className="border-[2px] border-black p-3 w-80"/>
+                <br></br>
+                <input ref={refPassword} type="password" name="password"  className="border-[2px] border-black p-3 w-80"/>
                 <br></br>
                 <button className="bg-green-200 p-3 text-2xl rounded-2xl font-medium">Submit</button>
             </form>
